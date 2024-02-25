51fad73cccc74c1dfc649fc6dfdd6c3663231361
Darron Moraes <darron.dev@gmail.com>
2024-02-25 13:39:25 +0530

    initial commit to the project repo

---
+++ src/main/java/in/nineteen96/churchsys/ChurchSysApplication.java
@@ -0,0 +1,13 @@
+package in.nineteen96.churchsys;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChurchSysApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChurchSysApplication.class, args);
+	}
+
+}
---
+++ src/test/java/in/nineteen96/churchsys/ChurchsysApplicationTests.java
@@ -0,0 +1,13 @@
+package in.nineteen96.churchsys;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChurchsysApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
